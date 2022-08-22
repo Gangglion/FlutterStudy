5a6431467eba6e3056128c9cd2011a61f3023f74
SeungHyeob Han <o0tmdguq0o@gmail.com>
2022-08-22 15:51:01 +0900

    test/widget_test.dart 수정
    간단 수정 - 예제코드 이름 바뀐거 따라 수정함

--- flutter_study0818/test/widget_test.dart
+++ flutter_study0818/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_study0818/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(exApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

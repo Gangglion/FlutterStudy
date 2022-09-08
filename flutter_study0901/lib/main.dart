cabaa5d5214fdd6aff35c8a85e762b98c5c4ebd7
SeungHyeob Han <o0tmdguq0o@gmail.com>
2022-09-09 03:30:44 +0900

    9월 8일 플러터공부2
    BottomNavigationBar 와 위치, 정렬, 크기를 위한 위젯 6가지의 위젯들 공부함.(0901이지만 실제 공부하고 푸쉬하는 날은 9월8일)
    추후 푸쉬되는 프로젝트는 4번째이므로 flutter_study4가 될것임.

--- flutter_study0901/lib/main.dart
+++ flutter_study0901/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -101,33 +101,183 @@ class MyApp extends StatelessWidget {
 // 5.Appbar, TabBar, Tab, TabBarView
 // 위의 4가지를 조합하여 PageView 와 유사하지만 페이지와 탭이 연동되는 화면을 구성 할 수 있다.
 // 아래 코드에서는 Scaffold를 DefaultTabController로 감싸야 하는것에 주의하며 코드 구성을 잘 살펴보자
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 3,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Tab'),
+//           bottom: TabBar(
+//             // Scaffold의 bottom 프로퍼티에 TabBar 지정
+//             tabs: <Widget>[
+//               // tabs 프로퍼티에 Tab의 리스트 지정
+//               Tab(icon: Icon(Icons.tag_faces)),
+//               Tab(text: '메뉴2'),
+//               Tab(icon: Icon(Icons.info), text: '메뉴3'),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: <Widget>[
+//             Container(color:Colors.red),
+//             Container(color:Colors.green),
+//             Container(color:Colors.blue),
+//           ]
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// 6. BottomNavigationBar : 하단에 2~5개의 탭 메뉴를 구성할 수 있는 위젯. 보통 각 탭을 클릭하여 화면을 전환할 때 사용한다.
+// 최근에 ios 와 android에서 자주 사용하는 UI
+// Scaffold의 프로퍼티 중에서 bottomNavigatorBar 프로퍼티 정의 하고 하위 items에 bottomNavigatorItem 위젯 나열
+// 추가 : 하단 탭을 눌렀을 때 마다 화면을 전환하고, 해당 탭이 선택됨을 알려주려면, 화면을 다시 그려야 하기 때문에 StatefulWidget으로 만들어야 한다.
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: BottomNavigationBar(items: [
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.home),
+//           label : 'Home',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.person),
+//           label : 'Profile',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.notifications),
+//           label : 'Notification',
+//         ),
+//       ]),
+//     );
+//   }
+// }
+
+// 7. 위치, 정렬, 크기를 위한 위젯 : 배치한 위젯의 위치를 정해줌. 위젯 크기, 위치, 정렬 등을 보조해주는 위젯이 존재한다.
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Tab'),
-          bottom: TabBar(
-            // Scaffold의 bottom 프로퍼티에 TabBar 지정
-            tabs: <Widget>[
-              // tabs 프로퍼티에 Tab의 리스트 지정
-              Tab(icon: Icon(Icons.tag_faces)),
-              Tab(text: '메뉴2'),
-              Tab(icon: Icon(Icons.info), text: '메뉴3'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Container(color:Colors.red),
-            Container(color:Colors.green),
-            Container(color:Colors.blue),
-          ]
-        ),
-      ),
+    return Scaffold(
+        appBar: AppBar(title: Text('7. 위치, 정렬, 크기를 위한 위젯')),
+        // 7-1.Center 위젯 : 중앙으로 정렬시키는 위젯. 상당히 자주 사용
+        // body: Center(
+        //   child: Container(
+        //     color: Colors.red,
+        //     width: 100,
+        //     height: 100,
+        //   ),
+        // ),
+
+        // 7-2. Padding 위젯 : 안쪽 여백을 표현할 때 사용하는 위젯
+        // body: Padding(
+        //   padding: const EdgeInsets.all(40.0),
+        //   // padding 의 값인 EdgeInsets 함수 중 .all() 은 네 방향 모두 지정한 값만큼 여백 표현
+        //   // .only(left : val1, top : val2, right : val3, bottom : val4)
+        //   // 는 상하좌우 중 원하는 방향에만 값 지정. 지정하지 않을 시 기본값 0.0.
+        //   // .fromTRB(leftVal, topVal, rightVal, bottomVal) 은 네 방향의 값을 각각 지정
+        //   child: Container( // Container 에 width와 height지정 안해주면 컨테이너 크기 전체로 나타난다.
+        //     color: Colors.red,
+        //   ),
+        // ),
+
+        // 7-3. Align : 자식 위젯의 방향을 정할 수 있는 위젯. 원하는 방향으로 위젯을 정렬할 때 사용.
+        // body : Align(
+        //   alignment: Alignment.centerRight,
+        //   // alignment 프로퍼티에 정의할 수 있는 값은 여러가지 있고, 정의된 클래스를 참고하자
+        //   // 대충 형식은 상단/중간/하단 + 왼쪽/중앙/오른쪽 의 조합으로 정의되어 있다. 예)중앙오른쪽 : centerBottom
+        //   child:Container(
+        //     color: Colors.red,
+        //     width : 100,
+        //     height : 100,
+        //     ),
+        //   )
+
+        // 7-4. Expanded : 자식 위젯의 크기를 최대한으로 확장시켜주는 위젯.
+        // 여러 위젯에 동시 적용시 flex 프로퍼티에 정숫값을 지정하여 비율을 정할 수 있고, 기본값은 1이다.
+        // 비율을 정하지 않으면 1:1:1...로 정의될 것이다.
+        // 일정 크기로 계속 확대해 주는것이 아닌, 화면 크기는 정해져있고 그 안에서 위젯이 늘어나도 화면을 넘어가지 않고 정해진 비율대로 한 화면에 위젯을 모두 표현함
+        // body: Column(
+        //   children: <Widget>[
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.red,
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.orange,
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.yellow,
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.green,
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.blue,
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.blueGrey,
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.purple,
+        //       ),
+        //     )
+        //   ],
+        // )
+
+        // 7-5. SizedBox : 위젯 중에는 크기와 관련된 프로퍼티, 즉 크기설정을 할 수 없는 위젯이 많은데 그러한 위젯을 특정크기로 만들고 싶을 때 사용한다.
+        // width 와 height에 double 타입으로 값을 지정한다. 이 위젯을 child없이 사용하게 되면 단순한 여백을 표현하는데 사용할 수 있다.
+        // body:SizedBox(
+        //   width:100,
+        //   height:100,
+        //   child:Container( // Container 위젯 속성에 width 와 height를 지정하는 방법이 더욱 간편하지만, 대부분의 위젯은 크기지정 프로퍼티가 없기 때문에 SizedBox를 많이 활용한다.
+        //     color:Colors.red,
+        //     )
+        // )
+
+        // 7-6. Card : 카드형태의 모양을 제공하는 위젯. 기본적으로 크기가 0이기 떄문에 자식위젯의 크기에 따라 크기가 결정된다.
+        // card 형태의 모양 제공이라는 것은 둥근 모서리와 그림자효과가 포함된 박스를 제공한다는 것
+        // 내부 구조에 Container 와 card, column을 어떻게 조합하느냐에 따라서 다양한 UI배치를 할 수 있음. 아래에서는 카드위젯 내부에 컨테이너를 넣어서 그 컨테이너에 둥근 모서리효과와 그림자 효과를 적용시켜주었음
+        body: Center(
+          child: Card(
+              color:Colors.grey,
+              shape: RoundedRectangleBorder(
+                  // shape 프로퍼티는 카드 모양을 변경하는 방법을 제공. 여러가지가 있겠지만 클래스를 살펴봐도 못찾겠음. docs를 참고하자
+                  borderRadius: BorderRadius.circular(16.0) // 카드위젯 모서리의 둥근 정도를 실숫값으로 조절한다. 값이 클수록 더 둥글게 됨.
+              ),
+              elevation: 50.0, // 카드 위젯의 그림자 효과의 그림자의 깊이를 조절. 실수값
+              child: Container(
+                color:Colors.grey,
+                width:double.infinity,
+                height: 300.0,
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:<Widget>[
+                    Container(color:Colors.red,width:100,height:100)
+                  ]
+                )
+                )
+              )
+            )
     );
   }
 }
